6856bad1c1f6bfd05f2f56253df31f30c6b724c0
nigelnkomo <nigel.nkomo@proton.me>
2026-07-05 08:59:28 +0200

    feat: use for loop

--- fahr-cels-table.c
+++ fahr-cels-table.c
@@ -3,18 +3,9 @@
 /* print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300 */
 int main ()
 {
-	float lower, upper, step;
-	float fahr, cels;
+	int fahr;
 
-	lower = 0; /* lower limit of temperature scale */
-	upper = 300; /* upper limit */
-	step = 20; /* step size */
-
-	fahr = lower;
-
-	while (fahr <= upper) {
-		cels = (5.0/9.0) * (fahr-32.0);
-		printf ("%3.0f %6.1f\n", fahr, cels);
-		fahr = fahr + step;
+	for (fahr = 0; fahr <= 300; fahr = fahr + 20) {
+		printf ("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr-32));
 	}
 }
