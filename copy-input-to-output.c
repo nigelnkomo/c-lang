98d79f1d89ec07882c84fbdc6a8bfb299fa4acbf
nigelnkomo <nigel.nkomo@proton.me>
2026-07-05 12:24:51 +0200

    verify that getchar() != EOF is 0 or 1

--- copy-input-to-output.c
+++ copy-input-to-output.c
@@ -9,12 +9,14 @@
 
 #include <stdio.h>
 
-/* copy input to output */
+/* prove c is 0 or 1 */
 int main ()
 {
 	int c;
 
-	while ((c = getchar ()) != EOF) { /* Ctrl + D */
-		putchar (c);
+	while (c = getchar () != EOF) {
+		printf ("%d\n", c);
 	}
+
+	printf ("%d\n", c);
 }
