357e818e495cceb05de9486338e0cef090037c5c
nigelnkomo <nigel.nkomo@proton.me>
2026-07-06 04:43:13 +0200

    feat: use double for bigger numbers

--- character-count.c
+++ character-count.c
@@ -3,9 +3,9 @@
 /* count all characters in input before EOF */
 int main ()
 {
-	long nc;
+	double nc;
 
 	for (nc = 0; getchar () != EOF; ++nc)
 		;
-	printf ("%ld\n", nc);
+	printf ("%.0f\n", nc);
 }
