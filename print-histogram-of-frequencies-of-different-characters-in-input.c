b5e89e7834300710b95da15fca3b89f759a1dada
nigelnkomo <nigel.nkomo@proton.me>
2026-07-10 11:49:04 +0200

    print vertical histogram of frequencies of different characters in input

--- print-histogram-of-frequencies-of-different-characters-in-input.c
+++ print-histogram-of-frequencies-of-different-characters-in-input.c
@@ -14,10 +14,29 @@ int main ()
 		++freq[c];
 	}
 
+	// find max value
+	for (i = 0; i <= 127; ++i)
+		if (freq[i] > max)
+			max = freq[i];
+
 	// print histogram
 	for (i = 0; i <= 127; ++i) {
 		for (j = 0; j <= freq[i]; ++j)
 			printf ("\u2588");
+		putchar ('\n');
+	}
+
+	// print vertical histogram
+	putchar ('\n');
+	printf ("Vertical Histogram\n");
+	for (i = 1; i <= max; ++i) {
+		for (j = 0; j <= 127; ++j)
+			if (freq[j] > 0) {
+				printf ("\u2588");
+				--freq[j];
+			}
+			else if (freq [i] <= 0)
+				putchar (' ');
 		putchar ('\n');
 	}
 
