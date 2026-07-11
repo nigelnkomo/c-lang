2c908ddd4f3b0556ac05594ebb87f76c8c5d2baf
nigelnkomo <nigel.nkomo@proton.me>
2026-07-11 18:12:20 +0200

    feat: calculate the power of an integer

---
+++ power.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int power (int m, int n);
+
+int main ()
+{
+	int i;
+
+	for (i = 1; i <= 10; ++i)
+		printf ("%2d %4d %6d\n", i, power (2, i), power (-3, i));
+	return 0;
+}
+
+int power (int base, int n)
+{
+	int i, p;
+
+	p = 1;
+	for (i = 1; i <= n; ++i) {
+		p = p * base;
+	}
+
+	return p;
+}
